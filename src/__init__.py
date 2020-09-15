9e1d2fb80b1972db09104e16779b7aca1b028b0c
mkaychuks <sachufusi@gmail.com>
2020-09-15 20:19:22 +0100

    :rocket:

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_mail import Mail
 
 
 
@@ -10,6 +11,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '76aaa0abbe2803cd3e0cfc3b91ce500f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = '************' # placeholder for my details OOPs
+app.config['MAIL_PASSWORD'] = '*******' # replaced for security reason OOPs
+
+
 
 # init db
 db = SQLAlchemy(app)
@@ -22,5 +32,9 @@ bcrypt = Bcrypt(app)
 
 # init JWT
 jwt = JWTManager(app)
+
+# init mail
+mail = Mail(app)
+
 
 from src import urls
