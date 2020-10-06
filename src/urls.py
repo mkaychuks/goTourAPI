96040840f9266ed50cc69eba1048277453639bf4
mkaychuks <sachufusi@gmail.com>
2020-10-06 18:23:16 +0100

    boom :bomb:

--- src/urls.py
+++ src/urls.py
@@ -1,7 +1,8 @@
 from src import api
 
 from src.views import (
-    Register, Login, Contact, Logout, ForgotPassword, ResetPassword
+    Register, Login, Contact, Logout, ForgotPassword, ResetPassword,
+    Subscribe
 )
 
 
@@ -11,3 +12,4 @@ api.add_resource(Logout, '/auth/logout/', endpoint='logout')
 api.add_resource(ForgotPassword, '/auth/forgot_password/')
 api.add_resource(ResetPassword, '/auth/password_reset/')
 api.add_resource(Contact, '/contact/', endpoint='contact')
+api.add_resource(Subscribe, '/subscribe/', endpoint='subscribe')
